a85e8f1b1a2b93ab4c97ec8994bbe57995353f7c
Simon Leischnig <simon.leischnig@spleenlab.ai>
2025-02-21 09:21:08 +0100

    works somehow?

--- website/next.config.js
+++ website/next.config.js
@@ -9,6 +9,21 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '/hi' : '',
     NEXT_PUBLIC_BASE_URL: process.env.NODE_ENV === 'production' ? 'https://simlei.github.io' : 'http://localhost:3000'
+  },
+  webpack: (config, { isServer }) => {
+    // Force webpack to use the regular require function
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    });
+    return config;
   }
 }
 
